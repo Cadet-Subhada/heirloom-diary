6c061ae913bd5b8dd9502a84a1d8ca768bbfb8f8
Cadet-Subhada <mallicksubhada@gmail.com>
2026-02-16 16:12:07 +0530

    update

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import hashlib
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
@@ -210,7 +211,8 @@ def scratch(entry_id):
 # Run App
 # =======================
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
