d122feeae2b1959e82622a4fbc4b570aa1dfad2c
Cadet-Subhada <mallicksubhada@gmail.com>
2026-02-16 17:27:22 +0530

    update

--- app.py
+++ app.py
@@ -225,8 +225,7 @@ def scratch(entry_id):
 # Run App
 # =======================
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
